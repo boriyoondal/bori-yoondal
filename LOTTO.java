c1481380a3d00bd58a51418f306bfcd4ca7cb333
boriyoondal <91236189+boriyoondal@users.noreply.github.com>
2021-10-14 10:56:39 +0900

    Add files via upload

---
+++ LOTTO.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class 로또중복x {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rd = new Random();
+		int[] lotto = new int[6];
+		
+		for(int i=0; i<lotto.length; i++) {
+			lotto[i] = rd.nextInt(45)+1;
+			for(int j=0; j<i; j++) {
+				if(lotto[i]==lotto[j]) {
+					i--;
+					break;
+				}
+			}
+		}
+		for(int i=0; i<lotto.length; i++) {
+			System.out.println("행운의숫자  : "+lotto[i]);
+		}
+	
+	
+	
+	
+	}
+}
